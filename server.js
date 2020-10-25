5eaae0dcf31fdb667afeadcee07508f185f12eff
dagreatcode <dagreatcode@gmail.com>
2020-10-25 15:57:49 -0400

    process.env. needed to be added to the PORT for heroku

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
